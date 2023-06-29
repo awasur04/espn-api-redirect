875c6a5769ce5377b8a2c096c412068ee3d9a5ed
awasur04 <awasur04@yahoo.com>
2023-06-28 21:54:18 -0500

    removed eslint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const https = require('http');
-const { Team, Game, nflGameList } = require('./data.js');
 const { test_scores_api } = require('./config.json');
+const { insertAllGames } = require('./database.js');
+
+let nflGameList = [];
 
 const request = https.request(test_scores_api, (response) => 
 {
@@ -15,6 +17,7 @@ const request = https.request(test_scores_api, (response) =>
 		//Format our data response chunk into json format
 		const body = JSON.parse(data);
 		processGames(body);
+		insertAllGames(nflGameList);
 	});
 });
 
@@ -27,16 +30,13 @@ request.end();
 
 function processGames(body)
 {
-	for (let game of body.events)
-	{
-		let index = nflGameList.findIndex(x => x.matchId == game.id);
+	for (let game of body.events) {
+		let index = nflGameList.findIndex(x => x.game_id == game.id);
 
-		if (index == -1)
-		{
+		if (index == -1) {
 			addGame(game);
 		}
-		else
-		{
+		else {
 			updateGame(index, game);
 		}
 	}
@@ -62,5 +62,16 @@ function createGameObect(game)
 	let homeTeam = new Team(competiton.competitors[0].id, competiton.competitors[0].score);
 	let awayTeam = new Team(competiton.competitors[1].id, competiton.competitors[1].score);
 
-	return new Game(game.id, homeTeam, awayTeam, game.date, game.status.type.name);
+	return
+	{
+		game_id: game.id,
+		home_team_id: competiton.competitors[0].id,
+		away_team_id: competiton.competitors[1].id,
+		home_score: competiton.competitors[0].score,
+		away_score: competiton.competitors[1].score,
+		game_status: game.status.type.name,
+		game_time: game.date,
+		home_odds: 1.0,
+		away_odds: 2.0
+	};
 }
